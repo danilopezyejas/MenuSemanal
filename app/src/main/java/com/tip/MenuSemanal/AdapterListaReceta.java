d9deb2a86f2ecad749a8280005e9eac767416f6a
alejandro <aledampay@gmail.com>
2021-05-23 01:56:51 -0300

    agregando datos al recycledview

--- app/src/main/java/com/tip/MenuSemanal/AdapterListaReceta.java
+++ app/src/main/java/com/tip/MenuSemanal/AdapterListaReceta.java
@@ -1,5 +1,6 @@
 package com.tip.MenuSemanal;
 import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,13 +16,18 @@ import java.util.ArrayList;
 import Clases.Ingrediente;
 import Clases.Recetas;
 
+import static com.tip.MenuSemanal.R.color.*;
+import static com.tip.MenuSemanal.R.id.listaRecetas;
+import static com.tip.MenuSemanal.R.id.textView;
 
-public class AdapterListaReceta extends RecyclerView.Adapter<AdapterListaReceta.ViewHoldersDatos>  {
 
-    ArrayList<Recetas> ListaRecetas;
+public class AdapterListaReceta extends RecyclerView.Adapter<AdapterListaReceta.ViewHoldersDatos> implements View.OnClickListener {
 
-    public AdapterListaReceta(@NonNull ArrayList<Recetas> listaRecetas) {
-        ListaRecetas = listaRecetas;
+    ArrayList<Recetas> listaRecetas;
+    View.OnClickListener onclickl;
+
+    public AdapterListaReceta(@NonNull ArrayList<Recetas> lRecetas) {
+        listaRecetas = lRecetas;
     }
 
     @NonNull
@@ -30,34 +36,51 @@ public class AdapterListaReceta extends RecyclerView.Adapter<AdapterListaReceta.
 
 
     public AdapterListaReceta.ViewHoldersDatos onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        @SuppressLint("ResourceType") View view= LayoutInflater.from(parent.getContext()).inflate(R.id.listaRecetas,null,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycle_recetas, null, false);
 
         return new ViewHoldersDatos(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull AdapterListaReceta.ViewHoldersDatos holder, int position) {
-        holder.asignardatos(ListaRecetas.get(position).getNombre(),ListaRecetas.get(position).getDescripcion());
+        holder.asignardatos(listaRecetas.get(position).getNombre(),listaRecetas.get(position).getDescripcion());
+        holder.bindView();
     }
 
     @Override
     public int getItemCount() {
-        return ListaRecetas.size();
+        return listaRecetas.size();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
     }
 
     public class ViewHoldersDatos extends RecyclerView.ViewHolder {
         TextView txtnombre;
         TextView txtdescripcion;
+        View item;
 
         public ViewHoldersDatos(@NonNull @NotNull View itemView) {
             super(itemView);
+            this.item= itemView;
             txtnombre=itemView.findViewById(R.id.txtNombreReceta);
             txtdescripcion=itemView.findViewById(R.id.txtDescripcionReceta);
+
         }
+        public void bindView(){
+
+                item.setSelected(true);
+            }
 
         public void asignardatos(String nombre, String descripcion) {
             txtnombre.setText(nombre);
             txtdescripcion.setText(descripcion);
+
+
+
         }
     }
 }
--- app/src/main/java/com/tip/MenuSemanal/ui/recetas/RecetasFragment.java
+++ app/src/main/java/com/tip/MenuSemanal/ui/recetas/RecetasFragment.java
@@ -11,22 +11,52 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.tip.MenuSemanal.AdapterListaReceta;
 import com.tip.MenuSemanal.R;
+
+import java.util.ArrayList;
+
+import Clases.Recetas;
 
 import static androidx.navigation.Navigation.findNavController;
 
 public class RecetasFragment extends Fragment {
 
+
     private RecetasViewModel recetasViewModel;
+    private RecyclerView recyclerView;
+    private ArrayList<Recetas> recetas;
+
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
+
+        recetas = new ArrayList<Recetas>();
+        recetas.add(new Recetas("salsa de damasco","nada"));
+        recetas.add(new Recetas("arroz con leche","nada"));
+        recetas.add(new Recetas("pancito al guiso","nada"));
+
+
         recetasViewModel =
                 new ViewModelProvider(this).get(RecetasViewModel.class);
         View root = inflater.inflate(R.layout.fragment_recetas, container, false);
         //final TextView textView = root.findViewById(//id.text_recetas);
+
+        recyclerView = (RecyclerView) root.findViewById(R.id.rvlistaRecetas);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new AdapterListaReceta(recetas));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
         recetasViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
                     @Override
                     public void onChanged(@Nullable String s) {
